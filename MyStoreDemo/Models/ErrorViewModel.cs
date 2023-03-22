16b4d9d12d30ae91df14f864b483340bf9f38f21
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-03-22 23:26:19 +0600

    initialize commit

---
+++ MyStoreDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyStoreDemo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
